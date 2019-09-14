27b2294bbe979f1f1950ae7dd2ed2c4adeb0abe7
Dustin_chavez <dustin_chavez@cs104u-5f.acad.fullerton.edu>
2019-09-14 16:07:39 -0700

    business trip expenses

--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@ int main()
 {
   std::string location;
   std::string days;
+  double meals;
+  double hotel;
+  double total;
   std::cout << "where did you go on a business trip? ";
   std::cin >> location;
   std::cout << "How many days will the business trip be? ";
   std::cin >> days;
+  std::cout << "How much was the cost of the hotel? ";
+  std::cin >> hotel;
+  std::cout << "how much was the meals? ";
+  std::cin >> meals;
+
+  total = hotel + meals;
 
 
-  std::cout <<"The business trip was at "<< location << "\n";
-
-  std::cout << std::setw(10) << "location" << std::endl;
-  std::cout <<std::setw(10) << location << std::endl;
+  std::cout << std::setw(10) << "location" << std::setw(10) << "days" << std::setw(10) << "hotel" << std::setw(10) << "meals" << std::setw(10) << "total" << std::endl;
+  std::cout <<std::setw(10) << location << std::setw(10) << days << std::setw(10) << hotel << std::setw(10) << meals << std::setw(10) << total << std::endl;
 
 
 }
