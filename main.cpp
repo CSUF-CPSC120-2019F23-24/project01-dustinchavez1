708ce4e73c77e0664fed521d6d5cf4216a1ba676
Dustin_chavez <dustin_chavez@cs104u-6g.acad.fullerton.edu>
2019-09-14 15:34:47 -0700

    project

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
-// Name:
+// Name: Dustin Chavez
 // This program calculates and displays business expenses.
+
+#include <iostream>
+#include <iomanip>
+int main()
+{
+  std::string location;
+  std::string days;
+  std::cout << "where did you go on a business trip? ";
+  std::cin >> location;
+  std::cout << "How many days will the business trip be? ";
+  std::cin >> days;
+
+
+  std::cout <<"The business trip was at "<< location << "\n";
+
+  std::cout << std::setw(10) << "location" << std::endl;
+  std::cout <<std::setw(10) << location << std::endl;
+
+
+}
